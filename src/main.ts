b5527c53114510caedc524def910e947c644877a
Daniel Shields <daniel.shields@dmgmori-digital.com>
2025-03-21 08:53:18 +0100

    New main function that still needs to be broken up

--- src/main.ts
+++ src/main.ts
@@ -1,85 +1,18 @@
-// import { type Buffer, createBuffer, type BufferStyle } from './create-buffer';
-// import { keypressHandler$ } from './keypress-handler';
-// import { renderBuffer } from './render-buffer';
-// import './style.scss';
-//
-// const buffer: Buffer = [
-//     null,
-//     "\"Hello World\"",
-//     null,
-//     "Hello",
-//     null,
-//     null,
-//     null,
-//     "function y() {",
-//     "  let x = 6",
-//     "}",
-//     null,
-// ];
-//
-// const displayedBuffer = (buffer: Buffer, bufferStyle: BufferStyle): Buffer => {
-//     const topTrimmedBuffer = buffer.slice(bufferStyle.topStartIndex, bufferStyle.topStartIndex + bufferStyle.bufferHeight);
-//     const leftTrimmedBuffer = [];
-//     for (let i = 0; i < topTrimmedBuffer.length; i++) {
-//         const line = topTrimmedBuffer[i];
-//         if (line) {
-//             leftTrimmedBuffer.push(line.slice(bufferStyle.leftStartIndex, bufferStyle.leftStartIndex + bufferStyle.bufferWidth));
-//         } else {
-//             leftTrimmedBuffer.push(null);
-//         }
-//     }
-//     return leftTrimmedBuffer;
-// }
-//
-// function main() {
-//     const blockHeight = 19;
-//     const blockWidth = 9.64;
-//     let bufferStyle: BufferStyle = {
-//         blockHeight,
-//         blockWidth,
-//         fontSize: 16,
-//         bufferHeight: Math.floor(window.innerHeight / blockHeight),
-//         bufferWidth: Math.floor(window.innerWidth / blockWidth),
-//         topStartIndex: 0,
-//         leftStartIndex: 0,
-//         cursorPosition: [0, 0],
-//     };
-//
-//     const bufferData = displayedBuffer(buffer, bufferStyle);
-//     const app = createBuffer(document, bufferData, bufferStyle);
-//     if (app) {
-//         keypressHandler$(window).subscribe((event) => {
-//             const newBufferStyle = { ...bufferStyle, cursorPosition: [bufferStyle.cursorPosition[0], bufferStyle.cursorPosition[1]] as [number, number] };
-//             if (event.type === 'keydown') {
-//                 if (event.key === 'h') {
-//                     newBufferStyle.cursorPosition[1] = newBufferStyle.cursorPosition[1] - 1;
-//                 } else if (event.key === 'k') {
-//                     newBufferStyle.cursorPosition[0] = newBufferStyle.cursorPosition[0] - 1;
-//                 } else if (event.key === 'j') {
-//                     newBufferStyle.cursorPosition[0] = newBufferStyle.cursorPosition[0] + 1;
-//                 } else if (event.key === 'l') {
-//                     newBufferStyle.cursorPosition[1] = newBufferStyle.cursorPosition[1] + 1;
-//                 }
-//                 renderBuffer(app, [bufferData, bufferData], [bufferStyle, newBufferStyle])
-//                 bufferStyle = newBufferStyle;
-//             }
-//         });
-//     }
-// }
-//
-// main();
-
 import './style.scss';
 import { keypressHandler$ } from './keypress-handler';
 
 const monoCharWidth = 9.64;
 const monoCharHeight = 19;
 
+type Mode = 'normal' | 'insert' | 'visual' | 'visual-line' | 'visual-block' | 'command';
+
 type BufferData = {
     buffer: string; // the file's contents
     bufferAsArray: string[]; // the file's contents as an array for lookup
+    mode: Mode; // the mode the editor
     scrollPositionY: number; // how far down the buffer is scrolled based on the fist shown line
     scrollPositionX: number; // how far right the buffer is scrolled based on the first shown column
+    lastCursorXPosition: number; // the last known cursor position on the x axis
     cursorPosition: { x: number; y: number; }; // where the cursor is in the file
 };
 
@@ -110,32 +43,62 @@ const moveCursor = (bufferData: BufferData) => {
 }
 
 function newMain() {
-    // * render full buffer in div with overflow hidden
-    //   * manually scroll the buffer when needed
-    //   * render cursor in the correct position based on where it is in the file along with where the buffer is currently scrolled to
-    //     * Cursor position can be calculated by the current position and the file's scroll position
-    // * Handle resizes by debouncing until stable and then re-rendering the whole all using the logic from the old example
     const file = 'Hello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello World\n\nHello\nWorld\n!\n\n\nfunction y() {\n  let x = 6\n}\n\n\n';
     let bufferData: BufferData = {
         buffer: file,
         bufferAsArray: file.split('\n'),
+        mode: 'normal',
         scrollPositionY: 0,
         scrollPositionX: 0,
+        lastCursorXPosition: 0,
         cursorPosition: { y: 0, x: 0 },
     };
     const app = renderBuffer(bufferData);
     if (app) {
         keypressHandler$(window).subscribe((event) => {
+            // INFO: This should look at the key pressed and wait a small amount of time to wait for another key if there is a key binding that starts with the current macro
+            // We could also keep a list of key binds as a Record<string, (d: BufferData) => BufferData)>
+            // We could also try to make this performant and use some sort of tree structure to look up the partial key binds
             const newBufferData: BufferData = { ...bufferData, cursorPosition: { ...bufferData.cursorPosition } };
             if (event.type === 'keydown') {
+                let newCursorPositionX = bufferData.cursorPosition.x;
+                let newCursorPositionY = bufferData.cursorPosition.y;
+                const handleXOnYMovement = (newY: number) => {
+                    if (bufferData.lastCursorXPosition > bufferData.bufferAsArray[newY].length) {
+                        newCursorPositionX = bufferData.bufferAsArray[newY].length - 1;
+                        if (newCursorPositionX < 0) {
+                            newCursorPositionX = 0;
+                        }
+                    } else {
+                        newCursorPositionX = bufferData.lastCursorXPosition;
+                    }
+                }
                 if (event.key === 'h') {
-                    newBufferData.cursorPosition.x = newBufferData.cursorPosition.x - 1;
-                } else if (event.key === 'k') {
-                    newBufferData.cursorPosition.y = newBufferData.cursorPosition.y - 1;
-                } else if (event.key === 'j') {
-                    newBufferData.cursorPosition.y = newBufferData.cursorPosition.y + 1;
+                    if (newCursorPositionX - 1 >= 0) {
+                        newCursorPositionX = newBufferData.cursorPosition.x - 1;
+                    }
+                    newBufferData.lastCursorXPosition = newCursorPositionX;
+                    newBufferData.cursorPosition.x = newCursorPositionX;
                 } else if (event.key === 'l') {
-                    newBufferData.cursorPosition.x = newBufferData.cursorPosition.x + 1;
+                    if (newCursorPositionX + 1 < bufferData.bufferAsArray[bufferData.cursorPosition.y].length) {
+                        newCursorPositionX = newBufferData.cursorPosition.x + 1;
+                    }
+                    newBufferData.lastCursorXPosition = newCursorPositionX;
+                    newBufferData.cursorPosition.x = newCursorPositionX;
+                } else if (event.key === 'k') {
+                    if (newCursorPositionY - 1 >= 0) {
+                        newCursorPositionY = newBufferData.cursorPosition.y - 1;
+                    }
+                    handleXOnYMovement(newCursorPositionY);
+                    newBufferData.cursorPosition.y = newCursorPositionY;
+                    newBufferData.cursorPosition.x = newCursorPositionX;
+                } else if (event.key === 'j') {
+                    if (newCursorPositionY + 1 < bufferData.bufferAsArray.length - 1) {
+                        newCursorPositionY = newBufferData.cursorPosition.y + 1;
+                    }
+                    handleXOnYMovement(newCursorPositionY);
+                    newBufferData.cursorPosition.y = newCursorPositionY;
+                    newBufferData.cursorPosition.x = newCursorPositionX;
                 }
                 moveCursor(newBufferData)
                 bufferData = newBufferData;
